60fa7a6c7f0766be801d2694ec4806c09889dee1
meloa <hociniayoub7@gmail.com>
2025-05-14 20:47:18 +0200

    Ajout frontend complet

--- api/dbhello/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.log("hello function processed a request.");
-    context.res = {
-      body: { msg: "Hello Azure depuis Function !" }
-    };
-  };
-  
